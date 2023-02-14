04e42cf6cf1217f06d1d694af6b22631a2818471
yuwotmate <kaleb.nim@gmail.com>
2023-02-15 02:50:13 +0800

    Change Requirements Txt

--- CA2_devOps/tests/test_validity_prediction.py
+++ CA2_devOps/tests/test_validity_prediction.py
@@ -11,7 +11,8 @@ import requests
 import base64
 import json
 import pytest
-# Predict the class of an image
+
+#1. Predict the class of cifar100 image for vgg19 model
 @pytest.mark.parametrize(
     "test_image_paths", ["./tests/upload_test_files/test_image0.png","./tests/upload_test_files/test_image1.png","./tests/upload_test_files/test_image2.png","./tests/upload_test_files/test_image3.png","./tests/upload_test_files/test_image4.png"] # Add second model API here
 )
@@ -23,8 +24,20 @@ def test_prediction(client,test_image_paths):
    assert isinstance(predictions, str)
    # Check that the predicted_labels_prob 
 
+#1.2 Predict the class of cifar100 image for Cifar100Efficient model
+@pytest.mark.parametrize(
+    "test_image_paths", ["./tests/upload_test_files/test_image0.png","./tests/upload_test_files/test_image1.png","./tests/upload_test_files/test_image2.png","./tests/upload_test_files/test_image3.png","./tests/upload_test_files/test_image4.png"] # Add second model API here
+)
+def test_prediction(client,test_image_paths):
+   # Load the image
+   image = load_img(test_image_paths)
+   predictions,predicted_labels_prob = make_prediction(image,model_name="Cifar100Efficient")
+   # Check that the prediction is a string
+   assert isinstance(predictions, str)
+   # Check that the predicted_labels_prob 
 
-# Test if model is hosted and running
+
+# 2. Test if model is hosted and running
 @pytest.mark.parametrize(
     "model_API", ["https://vgg-19-cifar100-model.onrender.com/v1/models/VGG19_cifar100_classifier",'https://efficientnet-cifar100-model.onrender.com/v1/models/Cifar100Efficient'] # Add second model API here
 )
